8df9f6d1b750383eb2475c0e7ddd2bbdbfd7affa
Andrew Allen <asallen67@gmail.com>
2019-06-28 17:43:33 -0400

    done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -2,16 +2,55 @@ import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
-// Endpoint!
-const API = "http://localhost:3000/sushis"
+const pageLimit = 4
+const lastPage = Math.ceil(81/4)
+const API = `http://localhost:3000/sushis?_limit=${pageLimit}&_page=`
 
 class App extends Component {
+
+	state = {
+		wallet: 100,
+		sushis: [],
+		eaten: [],
+		page: 1
+	}
+
+	fetchSushi = () => {
+		fetch(API+this.state.page)
+		.then(resp => resp.json())
+		.then(resp => this.setState({
+			page: this.state.page+1,
+			sushis: resp
+		}))
+	}
+
+	componentDidMount(){
+		this.fetchSushi()
+	}
+
+	eatSushi = (sushi) => {
+		if(sushi.price <= this.state.wallet)
+			this.setState({
+				wallet: this.state.wallet - sushi.price,
+				eaten: [...this.state.eaten, sushi.id]
+			})
+		else
+			alert("You don't have enough money for that!")
+	}
+
+	moreSushi = (e) => {
+		if(this.state.page-1 < lastPage) {
+			this.fetchSushi()
+		}
+		else
+			alert("We're all out of sushi!")
+	}
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer moreSushi={this.moreSushi} eatSushi={this.eatSushi} sushis={this.state.sushis} eaten={this.state.eaten} />
+        <Table wallet={this.state.wallet} eaten={this.state.eaten} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -2,27 +2,22 @@ import React, { Fragment } from 'react'
 
 const Table = (props) => {
 
-  const renderPlates = (array) => {
-    return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
-    })
-  }
+  // const renderPlates = (array) => {
+  //   return array.map((x, index) => {
+  //     return <div className="empty-plate" style={{ top: -7 * index }}/>
+  //   })
+  // }
+
+  const renderPlates = (array) => array.map( (sushi, index) => <div className="empty-plate" key={index} style={{ top: (-10 * index), left: (Math.floor(Math.random()*(320-300) + 300)) }}/>)
 
   return (
     <Fragment>
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${props.wallet} remaining!
       </h1>
       <div className="table">
         <div className="stack">
-          {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
-          }
+          { renderPlates(props.eaten) }
         </div>
       </div>
     </Fragment>
