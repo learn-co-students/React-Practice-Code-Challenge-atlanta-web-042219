1496c1f88258cbeb79e09955a29d27844f18e533
tunkhine126 <tunkhine126@gmail.com>
2019-07-01 09:53:17 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,15 +3,51 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const pageLimit = 4
+const API = `http://localhost:3000/sushis?_limit=${pageLimit}&_page=`
+
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      sushis: [],
+      wallet: 100,
+      eaten: [],
+      page: 1
+    }
+  }
+
+  fetchSushi = (e) => {
+    fetch(API+this.state.page)
+    .then(res => res.json())
+    .then(sushi => this.setState({
+      sushis: sushi,
+      page: this.state.page + 1
+    }))
+  }
+
+    componentDidMount(){
+      this.fetchSushi()
+    }
+
+    eatSushi = (sushi) => {
+      console.log(sushi)
+      if(sushi.price <= this.state.wallet)
+        this.setState({
+          wallet: this.state.wallet - sushi.price,
+          eaten: [...this.state.eaten, sushi.id]
+        })
+      else
+        alert("Not enough money.")
+    }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer fetchSushi={this.fetchSushi} sushis={this.state.sushis} eatSushi={this.eatSushi} eaten={this.state.eaten}/>
+        <Table wallet={this.state.wallet} eaten={this.state.eaten}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
+
+const renderSushi = (props) => props.sushis.map(sushi => 
+  <Sushi 
+    key={sushi.id} 
+    sushi={sushi}
+    eatSushi={props.eatSushi}
+    eaten={props.eaten.includes(sushi.id)}
+  />)
 
 const SushiContainer = (props) => {
+  
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderSushi(props)} 
+        <MoreButton fetchSushi={props.fetchSushi}/>
       </div>
     </Fragment>
   )
